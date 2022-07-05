8eb23b9a8d22109d5a127d573fcc409ddb2200fc
CUONG1 NGUYEN VIET <nvcuong1@misa-group.vn>
2022-07-05 17:17:49 +0700

    thêm comment code

--- src/apis/BaseAPIConfig.js
+++ src/apis/BaseAPIConfig.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-let BaseAPIConfig = axios.create({ baseURL: "" });
+let BaseAPIConfig = axios.create({ baseURL: "" }); 
+/**
+ * Chú ý: Khi call api thậy thì mở comment ra, thay baseURL thành api Backend cần gọi
+ */
 // let BaseAPIConfig = axios.create({ baseURL: "http://localhost:56886/api/" });
 
 BaseAPIConfig.interceptors.response.use(
--- src/apis/EmployeeAPI.js
+++ src/apis/EmployeeAPI.js
@@ -6,15 +6,23 @@ class EmployeeAPI{
     getTest(){
         return BaseAPIConfig.get(`https://catfact.ninja/fact`);
     }
-
+/**
+ * lấy nhân viên theo id
+ * @param {*} empID id nv
+ * @returns 
+ */
     getEmployeeByID(empID){
         return BaseAPIConfig.get(`${this.controller}/getEmpById/${empID}`);
     }
 
    
-
+/**
+ * lưu nhân viên
+ * @param {*} emp 
+ * @returns 
+ */
    saveEmployee(emp){
-        return BaseAPIConfig.get(`${this.controller}/save`, emp);
+        return BaseAPIConfig.post(`${this.controller}/save`, emp);
     }
 }
 export default new EmployeeAPI();
